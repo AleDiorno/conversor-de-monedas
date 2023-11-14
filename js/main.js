2262bf3e7dfc2e39f76282bc0c264d6ad497f84c
Sencho <gabrieldiorno@gmail.com>
2023-11-13 21:33:50 -0300

    Pre-entrega 2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
 //alert()
 
-const tasaDolar = 1100
-const tasaEuro = 932
-const tasaLibra =1275
-const tasaYen= 2.33
-const tasaBitcoin= 10865262
+const tasasDeCambio = {
+    Dolar: 1100,    
+    Euro: 932, 
+    Libra: 1275, 
+    Yen: 2.33,  
+    Bitcoin: 10865262
+};
+const monedas = ['Dolar', 'Euro', 'Libras', 'Yenes', 'Bitcoin']
 let continuar = true;
 
 while (continuar) {
 let pesos = prompt("Ingrese cantidad de Pesos $ a convertir");
-let moneda = prompt("A que moneda quiere intercambiar sus pesos: 1) Dolares , 2) Euros, 3) Libras, 4) Yenes, 5) Bitcoins")
+let moneda = prompt("A que moneda quiere intercambiar sus pesos:" + monedas.join(" ,"));
 
-if(moneda === "1"){
-    console.log(pesos + " pesos son " + Number(pesos)/tasaDolar + " dolares");
-}else if(moneda === "2"){
-    console.log(pesos + " pesos son " + Number(pesos)/tasaEuro + " euros");
-}else if (moneda === "3"){
-    console.log(pesos + " pesos son " + Number(pesos)/tasaLibra + " libras");
-}else if (moneda === "4"){
-    console.log(pesos + " pesos son " + Number(pesos)/tasaYen + " yenes");
-}else if (moneda === "5"){
-    console.log(pesos + " pesos son " + Number(pesos)/tasaBitcoin + " bitcoins");
+if(moneda === "Dolar"){
+    alert(pesos + " pesos son " + Number(pesos)/tasasDeCambio.Dolar + " dolares");
+}else if(moneda === "Euro"){
+    alert(pesos + " pesos son " + Number(pesos)/tasasDeCambio.Euro + " euros");
+}else if (moneda === "Libras"){
+    alert(pesos + " pesos son " + Number(pesos)/tasasDeCambio.Libra + " libras");
+}else if (moneda === "Yenes"){
+    alert(pesos + " pesos son " + Number(pesos)/tasasDeCambio.Yen + " yenes");
+}else if (moneda === "Bitcoin"){
+    alert(pesos + " pesos son " + Number(pesos)/tasasDeCambio.Bitcoin + " bitcoins");
 }else("Moneda no aceptada")
 
 continuar = confirm("¿Quieres realizar otra conversion?")
 }
-
-
